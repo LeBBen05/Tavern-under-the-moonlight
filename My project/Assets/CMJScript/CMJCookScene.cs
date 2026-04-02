10c9912af430ef6e34cfe97e0a6e0f8a74395e20
dcmin88 <ccddk0808@gmail.com>
2026-04-02 15:36:27 +0900

    요리
    재료 추가

--- My project/Assets/CMJScript/CMJCookScene.cs
+++ My project/Assets/CMJScript/CMJCookScene.cs
@@ -3,52 +3,118 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[System.Serializable]
+public class MenuImageGroup
+{
+    public GameObject[] images;
+}
+
 public class CMJCookScene : MonoBehaviour
 {
-    public GameObject ClickMenuUI; //클릭창
-    public bool iSAliveClick = false; //클릭창 상태
+    public GameObject ClickMenuUI;
     public GameObject AddButton;
-    public bool isAliveAdd = false; //클릭창 상태
-    // Start is called before the first frame update
+
+    public bool iSAliveClick = false;
+    public bool isAliveAdd = false;
+
+    public int currentMenuIndex = -1;
+
+    public MenuImageGroup[] menuImageGroups;
+
     void Start()
     {
-        
+        HideAllImages();
     }
 
-    // Update is called once per frame
     void Update()
     {
         ClickMenuUI.SetActive(iSAliveClick);
         AddButton.SetActive(isAliveAdd);
-
     }
-    public void OnMouseDown() //버튼이 눌릴경우 클릭창 활성화
+
+    void HideAllImages()
+    {
+        foreach (var group in menuImageGroups)
+        {
+            foreach (var img in group.images)
+            {
+                if (img != null) // null 체크 추가
+                {
+                    img.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void OnMouseDown()
     {
         iSAliveClick = true;
     }
-    public void LoadScene() //창아예 닫기(씬 변경)
+
+    public void LoadScene()
     {
-        if(iSAliveClick == false)
+        if (!iSAliveClick)
         {
             SceneManager.LoadScene("TestMapScene");
         }
     }
-    public void Back() //창 닫기
-    {
-        iSAliveClick = false;
-        if(isAliveAdd == true)
-        {
-            isAliveAdd = false;
-        }
-    }
-    public void MenuClick()
-    {
-        isAliveAdd = true;
-    }
-    public void Add() //메뉴 추가
+
+    public void Back()
     {
         iSAliveClick = false;
         isAliveAdd = false;
+        currentMenuIndex = -1;
+
+        HideAllImages();
     }
 
+    // 메뉴 클릭
+    public void MenuClick(int index)
+    {
+        Debug.Log("=== 메뉴 클릭: " + index + " ===");
+
+        //index 체크
+        if (index < 0 || index >= menuImageGroups.Length)
+        {
+            Debug.LogError("index 범위 초과!");
+            return;
+        }
+
+        //같은 메뉴 다시 누르면 → 끄기
+        if (currentMenuIndex == index)
+        {
+            Debug.Log("같은 메뉴 다시 클릭 → 끄기");
+
+            HideAllImages();
+            currentMenuIndex = -1;
+            isAliveAdd = false;
+
+            return;
+        }
+
+        //다른 메뉴 클릭
+        isAliveAdd = true;
+        //기존 이미지 끄기
+        HideAllImages();
+        //현재 메뉴 저장
+        currentMenuIndex = index;
+        //선택된 메뉴 이미지 켜기
+        foreach (var img in menuImageGroups[index].images)
+        {
+            if (img != null)
+            {
+                img.SetActive(true);
+                Debug.Log("켜짐: " + img.name);
+            }
+        }
+
+    }
+
+    public void Add()
+    {
+        if (currentMenuIndex < 0) return;
+
+        iSAliveClick = false;
+        isAliveAdd = false;
+    }
 }
